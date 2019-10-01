6e15aff4b51b3a82aa7e6a7ebb21e00b35a5cb4c
etienneeidC2 <etienne@DESKTOP-0PGA4HQ.localdomain>
2019-10-01 08:34:38 +0300

    (Done)save new user, (WIP) retrieve user by email

---
+++ mydr-backend/functions/index.js
@@ -0,0 +1,29 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.saveUser = functions.https.onRequest( async (req, res) => {
+    let user = req.body.user;
+    let save = await admin.firestore().collection('users').add({user: user});
+    res.json({result: `User with ID: ${save.id} added.`});
+});
+
+exports.getUserByEmail = functions.https.onRequest( async (req, res) => {
+    let reqestEmail = req.query.email;
+    new Firebase(users)
+    .orderByChild('email')
+    .startAt(reqestEmail)
+    .endAt(reqestEmail)
+    .once('value', function(snap){
+        var foundUser = snap.val();
+        // console.log(foundUser) // output is correct now
+        res.json({result: foundUser});
+    });
+    
+});
