14635a1926738305c649039340d4283bf928f624
Etienne Eid <etienne@rawa.tv>
2019-10-03 10:13:48 +0300

    added validation for the functions and tried another way for querying the firestore db

--- mydr-backend/functions/index.js
+++ mydr-backend/functions/index.js
@@ -9,26 +9,45 @@ admin.initializeApp();
 // });
 
 exports.saveUser = functions.https.onRequest( async (req, res) => {
+    // --Validation--
+    if (req.method !== 'POST') throw 'Not Found';
+    let data = (req.body) || null;
+    if(!data){
+        return res.status(400).send({ error: 'Body missing' });
+    }
+    if (data.email) {
+        return res.status(400).send({ error: 'Email is required' });
+    }
+    if (data.fullName) {
+        return res.status(400).send({ error: 'FullName is required' });
+    }
+    if (data.ID) {
+        return res.status(400).send({ error: 'ID is required' });
+    }
+    if (data.UID) {
+        return res.status(400).send({ error: 'UID is required' });
+    }
+    // --End of Validation--
+
     let user = { 
-        email: req.body.Email,
-        fullName: req.body.FullName,
-        id: req.body.ID,
-        uid: req.body.UID
+        email: data.Email,
+        fullName: data.FullName,
+        id: data.ID,
+        uid: data.UID
     };
+
     let save = await admin.firestore().collection('users').add({user: user});
     res.json({result: `User with ID: ${save.id} added.`});
 });
 
 exports.getUserByEmail = functions.https.onRequest( async (req, res) => {
+    if (req.method !== 'GET') throw 'Not Found';
+
+    if (!(req.query && req.query.email)) {
+        return res.status(400).send({ error: 'Email not provided' });
+    }
     let reqestEmail = req.query.email;
-    new Firebase(users)
-    .orderByChild('email')
-    .startAt(reqestEmail)
-    .endAt(reqestEmail)
-    .once('value', function(snap){
-        var foundUser = snap.val();
-        // console.log(foundUser) // output is correct now
-        res.json({result: foundUser});
-    });
-    
+    let usersRef = await admin.firestore().collection('users');
+    let get = usersRef.where('Email', '==', reqestEmail).get();
+    res.json({result: get});
 });
